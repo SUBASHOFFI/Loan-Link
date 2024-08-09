cc0dcdb008b456faf7b8dd73e33b510cb8777626
subash <redmisubashias@gmail.com>
2024-08-09 14:57:53 +0530

    basic structure of project

---
+++ back-end/loan-link/src/main/java/com/springboot/loan_link/LoanLinkApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.loan_link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanLinkApplication.class, args);
+	}
+
+}
---
+++ back-end/loan-link/src/test/java/com/springboot/loan_link/LoanLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.loan_link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
